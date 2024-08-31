c0a5ea659ff3c3fd831a82a70762baa968ba496f
Piyush Kukade <piyushkukade1@gmail.com>
2024-09-01 04:17:53 +0530

    Backend Code

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,17 +1,19 @@
-
-
 const jwt = require('jsonwebtoken');
 
 module.exports = function(req, res, next) {
-  // Get token from header
-  const token = req.header('x-auth-token');
-
-  // Check if not token
-  if (!token) {
+  // Get token from the Authorization header
+  const authHeader = req.header('Authorization');
+  
+  // Check if Authorization header is present
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
     return res.status(401).json({ msg: 'No token, authorization denied' });
   }
 
+  // Extract the token
+  const token = authHeader.split(' ')[1]; // Get the token part after "Bearer "
+
   try {
+    // Verify the token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded.user;
     next();

8d004455966d08fc7c89fefce7cf6a9450e1910f
Piyush Kukade <piyushkukade1@gmail.com>
2024-09-06 04:53:34 +0530

    API Updated

--- models/Class.js
+++ models/Class.js
@@ -9,32 +9,14 @@
 // }, { timestamps: true });
 
 // module.exports = mongoose.model('Class', classSchema);
-
 const mongoose = require('mongoose');
 
 const classSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    year: {
-        type: Number,
-        required: true,
-    },
-    teacher: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Teacher',
-    },
-    studentFees: {
-        type: Number,
-        required: true,
-    },
-    students: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Student',
-    }],
+  name: { type: String, required: true },
+  year: { type: Number, required: true },
+  teacherName: { type: String, required: true },
+  studentFees: { type: Number, required: true },
+  students: [{ type: String }] // Ensure this is correctly defined to accept an array of strings
 });
 
-const Class = mongoose.model('Class', classSchema);
-
-module.exports = Class;
+module.exports = mongoose.model('Class', classSchema);

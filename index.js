bbec663ef660e06232ebeebdc5b05de6b5680286
Piyush Kukade <piyushkukade1@gmail.com>
2024-09-06 22:48:25 +0530

    API Backend Updated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
-const authRoutes = require('./routes/authRoutes');
+const authRoutes = require('./routes/api/authRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;

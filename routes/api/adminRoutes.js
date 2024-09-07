f8e21f75de29b7ad3d3f9fad8a7df6e1a3b7bb54
Piyush Kukade <piyushkukade1@gmail.com>
2024-09-08 03:28:44 +0530

    API Backend Updated

--- routes/api/adminRoutes.js
+++ routes/api/adminRoutes.js
@@ -4,20 +4,22 @@ const jwt = require('jsonwebtoken');
 const Admin = require('../../models/Admin');
 const router = express.Router();
 require('dotenv').config();
-router.post('/admin-signup', async (req, res) => {
-  console.log("Admin Signup")
-  res.send('Signup route');
 
-  const { name, email, password, schoolName } = req.body;
+router.post('/admin-signup', async (req, res) => {
+
+  console.log("Admin Signup");
+
+  const { name, email, password, schoolName,role } = req.body;
 
   try {
     let admin = await Admin.findOne({ email });
     if (admin) return res.status(400).json({ message: 'Admin already exists' });
 
-    admin = new Admin({ name, email, password, schoolName });
+    admin = new Admin({ name, email, password, schoolName,role });
     await admin.save();
 
     const token = jwt.sign({ id: admin._id, role: admin.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    console.log(token);
     res.status(201).json({ token, message: 'Admin registered successfully' });
   } catch (error) {
     res.status(500).json({ message: 'Server error' });
